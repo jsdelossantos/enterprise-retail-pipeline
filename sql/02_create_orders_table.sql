fcd39751f8e4c6e9a93caefffcae50afd079105a
jsdelossantos <violetori99@gmail.com>
2026-05-13 11:02:34 +0800

    Create stg_orders table

---
+++ sql/02_create_orders_table.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS stg_orders;
+
+CREATE TABLE stg_orders (
+    order_id TEXT,
+    customer_id TEXT,
+    order_status TEXT,
+    order_purchase_timestamp TEXT,
+    order_approved_at TEXT,
+    order_delivered_carrier_date TEXT,
+    order_delivered_customer_date TEXT,
+    order_estimated_delivery_date TEXT
+);
